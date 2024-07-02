dfba86dbf7a2164a404a3c4368dde3afcd502e0c
Jarl Haggerty <jarl@liquidsky.com>
2024-07-02 17:18:29 -0400

    Zero lower version numbers

--- bump.py
+++ bump.py
@@ -10,9 +10,15 @@ def main():
   parts = [int(i) for i in version.split('.')]
   if sys.argv[1] == 'major':
     parts[0] += 1
+    parts[1] = 0
+    parts[2] = 0
   elif sys.argv[1] == 'minor':
+    parts[0] = 0
     parts[1] += 1
+    parts[2] = 0
   elif sys.argv[1] == 'patch':
+    parts[0] = 0
+    parts[1] = 0
     parts[2] += 1
 
   new_version = '.'.join(str(i) for i in parts)
