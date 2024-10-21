6140f786e8925ba660fb6cab69db0f643a72849b
jarl-haggerty <jarl@covetouscat.com>
2024-10-21 16:45:49 -0400

    Update upload-artifact.js

--- js/upload-artifact.js
+++ js/upload-artifact.js
@@ -2,7 +2,7 @@ const artifact = require('@actions/artifact').default
 console.log(artifact)
 const fs = require('fs');
 
-const files = fs.readdirSync(process.argv[2])
+const files = fs.readdirSync(process.argv[2]).map(f => 'dist/' + f)
 console.log('Uploading ' + JSON.stringify(files))
 
 artifact.uploadArtifact(
