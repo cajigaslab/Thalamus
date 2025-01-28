d2619652eea731b95ac152f5aac85675f9dd2555
Jarl Haggerty <Jarl.Haggerty@pennmedicine.upenn.edu>
2025-01-28 20:18:21 +0000

    Add ui-port argument to task controller

--- thalamus/task_controller/main_impl.py
+++ thalamus/task_controller/main_impl.py
@@ -74,6 +74,7 @@ def parse_args() -> argparse.Namespace:
   parser = argparse.ArgumentParser(description='Touch Task ROS node')
   parser.add_argument('-c', '--config', help='Config file location')
   parser.add_argument('-p', '--port', type=int, default=50050, help='GRPC port')
+  parser.add_argument('-u', '--ui-port', type=int, default=50051, help='UI GRPC port')
   parser.add_argument('-e', '--recorder-url', help='Recorder URL')
   parser.add_argument('-o', '--ophanim-url', help='Ophanim URL')
   parser.add_argument('-t', '--trace', action='store_true', help='Enable tracing')
@@ -128,7 +129,7 @@ async def async_main() -> None:
   task_controller_servicer = TaskControllerServicer()
   thalamus_pb2_grpc.add_ThalamusServicer_to_server(servicer, server)
   task_controller_pb2_grpc.add_TaskControllerServicer_to_server(task_controller_servicer, server)
-  listen_addr = f'[::]:50051'
+  listen_addr = f'[::]:{arguments.ui_port}'
 
   server.add_insecure_port(listen_addr)
   logging.info("Starting GRPC server on %s", listen_addr)
@@ -137,9 +138,9 @@ async def async_main() -> None:
   bmbi_native_filename = resource_filename('thalamus', 'native' + ('.exe' if sys.platform == 'win32' else ''))
   bmbi_native_proc = None
   bmbi_native_proc = await asyncio.create_subprocess_exec(
-      bmbi_native_filename, 'thalamus', '--port', str(arguments.port), '--state-url', 'localhost:50051', *(['--trace'] if arguments.trace else []))
+      bmbi_native_filename, 'thalamus', '--port', str(arguments.port), '--state-url', f'localhost:{arguments.ui_port}', *(['--trace'] if arguments.trace else []))
 
-  channel = grpc.aio.insecure_channel('localhost:50050')
+  channel = grpc.aio.insecure_channel(f'localhost:{arguments.port}')
   await channel.channel_ready()
   stub = thalamus_pb2_grpc.ThalamusStub(channel)
 
