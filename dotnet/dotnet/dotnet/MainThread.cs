bb1d36242ea6a0a8f4741287e2ddfcc87fd2d504
sleeve <sleeve@sleeve.com>
2025-08-13 18:10:51 -0400

    Add JsonPath from Newtonsoft

---
+++ dotnet/dotnet/dotnet/MainThread.cs
@@ -0,0 +1,50 @@
+﻿namespace dotnet
+{
+    public class MainThread : IDisposable
+    {
+        private Queue<Action> work;
+        private Thread workThread;
+        private bool running = true;
+        public MainThread()
+        {
+            workThread = new Thread(workConsumer);
+        }
+
+        void workConsumer()
+        {
+            while(running)
+            {
+                lock(this)
+                {
+                    while(work.Count == 0 && running)
+                    {
+                        Monitor.Wait(this, 1000);
+                    }
+                    foreach (var job in work)
+                    {
+                        job();
+                    }
+                    work.Clear();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock(this)
+            {
+                running = false;
+            }
+            workThread.Join();
+        }
+
+        public void Push(Action action)
+        {
+            lock(this)
+            {
+                work.Enqueue(action);
+                Monitor.Pulse(this);
+            }
+        }
+    }
+}
