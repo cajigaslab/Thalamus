f462c26fea97e9e94172879ad0e796ac5c022e89
Jarl Haggerty <jarlhaggerty@gmail.com>
2026-09-20 16:17:29 -0400

    Fix WAVE bug: crashes when switching from PUPIL to WAVE, WAVE used to only support one signal and read parameters from it's root config.  If it switch from a node that defines one of these parameters, in this case PUPIL and it's Frequency parameter, a crash happens.  Also, implement OffMainSignaler, a much simpler way to prevent off main deadlocking

--- src/thalamus/node_util.cpp
+++ src/thalamus/node_util.cpp
@@ -12,6 +12,66 @@
 
 namespace thalamus {
   namespace node {
+    
+    struct OffMainSignaler::Impl {
+      Node& node;
+      boost::asio::io_context& io_context;
+      bool blocked;
+      std::mutex mutex;
+      std::condition_variable cv;
+      Impl(Node& _node, boost::asio::io_context& _io_context)
+      : node(_node)
+      , io_context(_io_context)
+      , blocked(true) {}
+    };
+
+    OffMainSignaler::OffMainSignaler(Node& _node, boost::asio::io_context& _io_context)
+    : impl(new Impl(_node, _io_context)) {} 
+      
+    void OffMainSignaler::block() {
+      std::lock_guard<std::mutex> lock(impl->mutex);
+      impl->blocked = true;
+      impl->cv.notify_one();
+    }
+
+    void OffMainSignaler::unblock() {
+      std::lock_guard<std::mutex> lock(impl->mutex);
+      impl->blocked = false;
+      impl->cv.notify_one();
+    }
+
+    bool OffMainSignaler::signal() {
+      std::unique_lock<std::mutex> lock(impl->mutex);
+      if(impl->blocked) {
+        return false;
+      }
+
+      auto done = false;
+      auto post_to_main = !impl->node.ready.empty();
+      if(post_to_main) {
+        boost::asio::post(impl->io_context, [_impl=this->impl,&done] {
+          std::lock_guard<std::mutex> lock2(_impl->mutex);
+          if(_impl->blocked) {
+            return;
+          }
+          _impl->node.ready(&_impl->node);
+          done = true;
+          _impl->cv.notify_one();
+        });
+      }
+
+      if(impl->node.ready_multithreaded) {
+        (*impl->node.ready_multithreaded)(&impl->node);
+      }
+
+      if(post_to_main) {
+        impl->cv.wait(lock, [_impl=this->impl,&done] {
+          return _impl->blocked || done;
+        });
+      }
+      return !impl->blocked;
+    }
+
     boost::signals2::connection connect_ready_multithreaded(Node* node, std::function<void(Node*)> callback) {
       if(node->ready_multithreaded) {
         return node->ready_multithreaded->connect(callback);
